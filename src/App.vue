837f33e6033ca9ab27ee1f865f29276871e43469
andmaziarz <a.maziarz9@outlook.com>
2023-11-28 10:36:17 +0100

    api

--- src/App.vue
+++ src/App.vue
@@ -14,16 +14,14 @@ export default {
     },
     methods: {
         onTermChange(searchTerm){
-            // axios.get('https://www.googleapis.com/youtube/v3/search',{
-            //     params: {
-            //         key: process.env.VUE_APP_API_KEY,
-            //         type: 'video',
-            //         part: 'snippet',
-            //         q: searchTerm
-            //     }
-            // })
-            console.log(process.env.VUE_APP_API_KEY)
-
+            axios.get('https://www.googleapis.com/youtube/v3/search',{
+                params: {
+                    key: process.env.VUE_APP_API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchTerm
+                }
+            }).then(response => console.log(response))
         }
 
     }
